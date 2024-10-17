7be034b95a4cd7f9934fbf7f768d45afc5e123d4
ondrejfila6969 <ofila12@seznam.cz>
2024-10-17 09:27:26 +0200

    first commit

---
+++ models/products.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    description: {type: String, required: true}
+})
+
+module.exports = mongoose.model("Product", schema);
